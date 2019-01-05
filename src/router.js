00f2cf3f19e2dd8a59ad2e7da110769bf5ae9325
pomverte <vietnem@yahoo.fr>
2019-01-05 21:47:56 +0100

    add view with bootstrap component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import BootstrapTest from "./views/BootstrapTest.vue";
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       name: "home",
       component: Home
     },
+    {
+      path: "/test",
+      name: "test",
+      component: BootstrapTest
+    },      
     {
       path: "/about",
       name: "about",
---
+++ src/views/BootstrapTest.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
+      <p>For more information visit website</p>
+      <b-btn variant="primary" href="#">More Info</b-btn>
+    </b-jumbotron>
+  </div>
+</template>
