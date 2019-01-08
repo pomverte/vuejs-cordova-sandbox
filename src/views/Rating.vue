31ad01af8bd402210abf168eb01dc3f2a66848c9
hvle <ha.hong.viet.le@cgi.com>
2019-01-08 15:13:10 +0100

    feature: rating screen

---
+++ src/views/Rating.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <b-nav>
+      <b-nav-item disabled>{{ pratiquant }} - {{ trial }}</b-nav-item>
+    </b-nav>
+    <b-table striped hover :items="items" :fields="fields">
+      <template slot="note" slot-scope="data">
+        <b-form-input v-model="data.item.note" type="text"></b-form-input>
+      </template>
+    </b-table>
+    <b-button variant="">Annuler</b-button> |
+    <b-button variant="primary" v-b-tooltip.hover title="Enregistrer la note et revenir la à liste des pratiquants">Valider</b-button> |
+    <b-button variant="success" v-b-tooltip.hover title="Enregistrer la note et passer au pratiquant suivant">Valider et suivant</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      trial: "Vat",
+      pratiquant: "Leonard Leakey Hofstadter",
+      fields: ["nom", "note"],
+      items: [
+        { nom: "Mika", note: "" },
+        { nom: "Zazie", note: "" },
+        { nom: "Jenifer", note: "" },
+        { nom: "Garou", note: "" }
+      ]
+    };
+  }
+};
+</script>
