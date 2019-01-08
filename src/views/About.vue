831868546fd0f8650b08ffcd6dee1c8ee700eb61
hvle <ha.hong.viet.le@cgi.com>
2019-01-08 15:42:29 +0100

    add link

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h1>This is an about page</h1>
     <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
       <p>For more information visit website</p>
-      <b-btn variant="primary" href="#">More Info</b-btn>
+      <b-btn variant="primary" href="https://bootstrap-vue.js.org/docs/components/jumbotron">More Info</b-btn>
     </b-jumbotron>
   </div>
 </template>
