376257dab8907e78e71759ebff15ddabdf9b5029
hvle <ha.hong.viet.le@cgi.com>
2019-01-08 15:41:13 +0100

    feat: trial

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Rating from "./views/Rating.vue";
+import Trial from "./views/Trial.vue";
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/trial",
+      name: "trial",
+      component: Trial
     },
     {
       path: "/rating",
---
+++ src/views/Trial.vue
@@ -0,0 +1,40 @@
+<!-- -->
+<template>
+  <div class="container">
+    <b-table striped hover :items="items" :fields="fields">
+      <template slot="ordre" slot-scope="row">
+        {{ row.index }}
+      </template>
+      <template slot="action" slot-scope="row">
+        <b-button @click="ratePratiquant(row.item)" variant="outline-primary">Modifier</b-button>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: ["ordre", "nom", "note", "action"],
+      items: [
+        { nom: "Leonard Leakey Hofstadter", note: "10" },
+        { nom: "Sheldon Cooper", note: "11" },
+        { nom: "Penny", note: "14" },
+        { nom: "Howard Wolowitz", note: "12" },
+        { nom: "Raj Koothrappali", note: "10" },
+        { nom: "Leslie Winkle", note: "11" },
+        { nom: "Bernadette Rostenkowski-Wolowitz", note: "13" },
+        { nom: "Amy Farrah Fowler", note: "" },
+        { nom: "Stuart Bloom", note: "" },
+        { nom: "Emily Sweeney", note: "" }
+      ]
+    };
+  },
+  methods: {
+    ratePratiquant: function(pratiquant) {
+      alert("TODO Evaluer " + pratiquant.nom);
+    }
+  }
+};
+</script>
