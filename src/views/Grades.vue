418d5d6d30f6c4f7688fb0e32b0568e90bc3df8a
pomverte <vietnem@yahoo.fr>
2019-01-15 16:18:24 +0100

    rename var

--- src/views/Grades.vue
+++ src/views/Grades.vue
@@ -2,15 +2,15 @@
   <div class="container">
     <b-card-group columns>
       <!-- https://github.com/vuejs/vetur/issues/261#issuecomment-429217527 -->
-      <b-card v-for="(grade, key) in grades" :key="grade"
+      <b-card v-for="(grade, gradeKey) in grades" :key="grade"
               v-bind:title="grade.nom"
               style="max-width: 20rem;"
               v-bind:img-src="grade.image"
               img-alt="Image"
               img-top>
         <b-list-group flush>
-          <b-list-group-item v-for="epreuve in grade.epreuves" :key="epreuve">
-            <router-link :to="{ name: 'epreuve', params: { gid: key, epreuve: zadaz }}">{{ epreuve.nom }}</router-link>
+          <b-list-group-item v-for="(epreuve, epreuveKey) in grade.epreuves" :key="epreuve">
+            <router-link :to="{ name: 'epreuve', params: { gid: gradeKey, epreuve: epreuveKey }}">{{ epreuve.nom }}</router-link>
           </b-list-group-item>
         </b-list-group>
         <b-card-footer>Nombre de pratiquants : {{ Object.keys(grade.pratiquants).length }}</b-card-footer>
