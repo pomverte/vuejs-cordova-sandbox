edc7c598a63ebb9e35b5cbadb096e8601b6ba06c
pomverte <vietnem@yahoo.fr>
2019-01-19 20:33:16 +0100

    init firebase

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@ import router from "./router";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 
+import firebase from "firebase";
+
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyA2NAbOmww4amDi43aGzwgF5e0By5ALm34",
+  authDomain: "fir-test-7c5ed.firebaseapp.com",
+  databaseURL: "https://fir-test-7c5ed.firebaseio.com",
+  projectId: "fir-test-7c5ed"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
